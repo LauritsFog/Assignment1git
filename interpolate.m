ef5eaf19af99b1d8eba362cef5562e72c9d2f1e8
simoneiriksson <simoneiriksson@gmail.com>
2024-03-11 18:12:47 +0100

    Rebuild interpolate.m

--- interpolate.m
+++ interpolate.m
@@ -1,11 +1,18 @@
 function R = interpolate(Rc,m)
 
-    if m>1
-        Rc = reshape(Rc,(m-1)/2+2,(m-1)/2+2);
-        R = interp2(interp2(Rc));
-        R = R(:);
-    else
-        R = ones(3,3)*Rc;
-        R=R(:);
-    end
+    tmp = zeros(m+2, m+2);
+    tmp(2:m+1, 2:m+1) = reshape(Rc, m, m);
+    
+    R = zeros(2*m+1, 2*m+1);
+    % fill in original values
+    R(2:2:2*m, 2:2:m*2) = tmp(2:m+1, 2:m+1);
+    % fill in values with vertical neighbors
+    R(1:2:2*m+1, 2:2:m*2) = (tmp(1:m+1, 2:m+1) + tmp(2:m+2, 2:m+1))/2;
+    
+    % fill in values with horizontal neighbors
+    R(2:2:2*m, 1:2:m*2+1) = (tmp(2:m+1, 1:m+1) + tmp(2:m+1, 2:m+2))/2;
+    
+    % fill in values with diagonal neighbors
+    R(1:2:2*m+1, 1:2:m*2+1) = (tmp(1:m+1, 1:m+1) + tmp(2:m+2, 1:m+1) + tmp(1:m+1, 2:m+2) + tmp(2:m+2, 2:m+2))/4;
+    R=R(:);
 end
